0b38b4bfe2ae35a7407fa533e70d0556e191a362
RD42 <42702181+dashr9230@users.noreply.github.com>
2023-11-30 23:17:04 +0800

    Add CameraSize stub

---
+++ src/camera.c
@@ -0,0 +1,15 @@
+
+#include <rwcore.h>
+
+#include "camera.h"
+
+/*
+ *****************************************************************************
+ */
+void
+CameraSize(RwCamera *camera, RwRect *rect, RwReal viewWindow, RwReal aspectRatio)
+{
+    // TODO: CameraSize
+
+    return;
+}
---
+++ src/camera.h
@@ -0,0 +1,18 @@
+
+#if (!defined(_CAMERA_H))
+#define _CAMERA_H
+
+#ifdef    __cplusplus
+extern "C"
+{
+#endif                          /* __cplusplus */
+
+extern void 
+CameraSize(RwCamera *camera, RwRect *rect, RwReal viewWindow, RwReal aspectRatio);
+
+#ifdef    __cplusplus
+}
+#endif                          /* __cplusplus */
+
+#endif /* (!defined(_CAMERA_H)) */
+
