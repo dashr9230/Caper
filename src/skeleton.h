d5bbc63e2be47d05396231182a3e884fe71b1071
RD42 <42702181+dashr9230@users.noreply.github.com>
2023-11-30 23:51:06 +0800

    Implement RsKeyCodes enum

--- src/skeleton.h
+++ src/skeleton.h
@@ -96,9 +96,65 @@ struct RsGlobalType
 
 enum RsKeyCodes
 {
-	/* TODO: RsKeyCodes */
+    rsESC       = 128,
 
-	rsNULL       = 255
+    rsF1        = 129,
+    rsF2        = 130,
+    rsF3        = 131,
+    rsF4        = 132,
+    rsF5        = 133,
+    rsF6        = 134,
+    rsF7        = 135,
+    rsF8        = 136,
+    rsF9        = 137,
+    rsF10       = 138,
+    rsF11       = 139,
+    rsF12       = 140,
+
+    rsINS       = 141,
+    rsDEL       = 142,
+    rsHOME      = 143,
+    rsEND       = 144,
+    rsPGUP      = 145,
+    rsPGDN      = 146,
+
+    rsUP        = 147,
+    rsDOWN      = 148,
+    rsLEFT      = 149,
+    rsRIGHT     = 150,
+
+    rsPADINS    = 151,
+    rsPADDEL    = 152,
+    rsPADHOME   = 153,
+    rsPADEND    = 154,
+    rsPADPGUP   = 155,
+    rsPADPGDN   = 156,
+
+    rsPADUP     = 157,
+    rsPADDOWN   = 158,
+    rsPADLEFT   = 159,
+    rsPADRIGHT  = 160,
+
+    rsNUMLOCK   = 161,
+    rsDIVIDE    = 162,
+    rsTIMES     = 163,
+    rsMINUS     = 164,
+    rsPLUS      = 165,
+    rsPADENTER  = 166,
+    rsPAD5      = 167,
+
+    rsBACKSP    = 168,
+    rsTAB       = 169,
+    rsCAPSLK    = 170,
+    rsENTER     = 171,
+    rsLSHIFT    = 172,
+    rsRSHIFT    = 173,
+    rsLCTRL     = 174,
+    rsRCTRL     = 175,
+    rsLALT      = 176,
+    rsRALT      = 177,
+
+    rsNULL       = 255
 };
 typedef enum RsKeyCodes RsKeyCodes;
 
